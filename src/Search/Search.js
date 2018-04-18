885661a946ee7ea14f0ee8e34b1d255ca26f7afb
ffraniel <francis.whitehead@googlemail.com>
2018-04-18 11:28:42 +0100

    added state to search bar

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import './Search.css';
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            searchValue:''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            searchValue:event.target.value
+        })
+    }
+    handleSubmit(event){
+        event.preventDefault();
+        // make search
+        this.setState({
+            searchValue:''
+        })
+    }
+
     render(){
         return(
-        <form className="searchForm">
-            <input type="text" />
+        <form className="searchForm" onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.searchValue} onChange={this.handleChange}/>
             <input type="submit" />
         </form>
         )
