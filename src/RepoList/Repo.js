9a50c46cb39109c7e8d751bdfb3ddc9e0a25f94f
ffraniel <francis.whitehead@googlemail.com>
2018-04-18 21:20:59 +0100

    passed search result as props down to list iterator

--- src/RepoList/Repo.js
+++ src/RepoList/Repo.js
@@ -3,11 +3,13 @@ import './Repo.css';
 
 class Repo extends Component {
     render() {
+        let repo = this.props.repo;
         return(
             <div className="repo-list">
-                <h3 className="repo-list-name">Strava Hack</h3>
-                <p className="repo-list-author">Christine Bobbingtong</p>
-                <p className="repo-list-forks">Number of Forks: 7</p>
+            {console.log(this.props.repo)}
+                <h3 className="repo-list-name">{repo.name}</h3>
+                <p className="repo-list-author">{repo.owner.login}</p>
+                <p className="repo-list-forks">Number of Forks: {repo.forks}</p>
                 <p className="repo-list-readme">This project aims to add an element of...</p>
                 <button className="expandButton">↓</button>
                 <button className="repoButton">Details</button>
--- src/RepoList/RepoList.js
+++ src/RepoList/RepoList.js
@@ -4,21 +4,30 @@ import Repo from './Repo';
 import PaginationElement from './PaginationElement';
 import ListError from './ListError';
 import BlankList from '../BlankList/BlankList';
+import PropTypes from 'prop-types';
 
 class RepoList extends Component {
+    constructor(props){
+        super(props);
+        
+    }
     render() {
         return (
             <div className="repoList">
-                <PaginationElement />
-                    <Repo />
-                    <Repo />
-                    <Repo />
-                    <Repo />
-                    <Repo />
-                <PaginationElement />
+                <PaginationElement repos={this.props.repos} />
+                {this.props.repos.map((repo, key)=>{
+                    return (
+                        <Repo repo={repo} key={key}/>
+                    )
+                })}
+                <PaginationElement repos={this.props.repos} />
             </div>
         )
     }
+}
+
+RepoList.propType = {
+    foundRepos: PropTypes.func
 }
 
 export default RepoList;
