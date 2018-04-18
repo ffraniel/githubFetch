88c2779993e121e990acb09c7204325aad8cfbdd
ffraniel <francis.whitehead@googlemail.com>
2018-04-18 10:55:32 +0100

    added router and route switching

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+
 import Header from './Header/Header';
 import RepoList from './RepoList/RepoList';
 import BlankList from './BlankList/BlankList';
 import RepoFull from './RepoFull/RepoFull';
-import ListError from './RepoList/ListError';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <RepoList />
-        {/* <BlankList /> */}
-        {/* <ListError /> */}
-        {/* <RepoFull /> */}
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Switch >
+            <Route exact path="/" component={RepoList} />
+            <Route path="/repo/:repoID" component={RepoFull} />
+            {/* <Route default component={BlankList}/> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/RepoList/RepoList.js
+++ src/RepoList/RepoList.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './RepoList.css';
 import Repo from './Repo';
+import PaginationElement from './PaginationElement';
+import ListError from './ListError';
+import BlankList from '../BlankList/BlankList';
 
 class RepoList extends Component {
     render() {
         return (
             <div className="repoList">
+                <PaginationElement />
                 <Repo />
                 <Repo />
                 <Repo />
                 <Repo />
                 <Repo />
+                <PaginationElement />
             </div>
         )
     }
