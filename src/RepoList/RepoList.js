555e5d9ed8ebf90e09a43dc081488a82c4f3f9e8
ffraniel <francis.whitehead@googlemail.com>
2018-04-19 09:20:37 +0100

    added display when no results in

--- src/RepoList/RepoList.js
+++ src/RepoList/RepoList.js
@@ -9,13 +9,17 @@ import PropTypes from 'prop-types';
 class RepoList extends Component {
     constructor(props){
         super(props);
+
         
     }
     render() {
         return (
             <div className="repoList">
+
                 <PaginationElement repos={this.props.repos} />
-                {this.props.repos.map((repo, key)=>{
+                {console.log(this.props)}
+                {this.props.repos.length === 0 && <BlankList />}
+                {this.props.repos && this.props.repos.map((repo, key)=>{
                     return (
                         <Repo repo={repo} key={key}/>
                     )
