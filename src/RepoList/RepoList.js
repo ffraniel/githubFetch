e6fc0fa203dce01f39c53145ea31c62acfdd9d8a
ffraniel <francis.whitehead@googlemail.com>
2018-04-23 10:33:01 +0100

    added pagination control

--- src/RepoList/RepoList.js
+++ src/RepoList/RepoList.js
@@ -12,31 +12,77 @@ class RepoList extends Component {
         super(props);  
         this.state={
             page:1,
-            numberOfPages:0
-        }      
+            reposToShow:[],
+            numberOfPages:null
+        } 
+        this.changePage = this.changePage.bind(this);     
     }
-    componentDidMount(){
-        this.updateState
+
+    componentWillReceiveProps(){
+        this.updatePages(this.props.repos);
+        this.splitRepos(this.props.repos, this.state.page);
     }
-    async updateState(){
-        let data = this.props.data;
-        let paginatedNumber = await paginateCalc(data);
+
+    updatePages(reposOnProps){
+        // get number of pages
+        let paginatedNumber = paginateCalc(reposOnProps);
         this.setState({
             numberOfPages:paginatedNumber
         })
     }
 
+    //call this when change page
+    splitRepos(allRepos, pageNumber){
+    // split results and pass the correct ones to render
+        let endValue = pageNumber * 10;
+        let startValue = endValue - 10;
+        let currentRepos = allRepos.slice(startValue, endValue);
+        this.setState({
+            reposToShow:currentRepos
+        })
+    }
+
+    changePage(newPageNumber){
+        let maxPageNum = Math.ceil(this.props.repos.length / 10);
+        let minPageNum = 1;
+        let pageNum = this.state.page;
+
+        if(newPageNumber === "down"){
+            if((pageNum - 1) >= minPageNum) {
+                pageNum--;
+                this.setState({
+                    page:pageNum
+                })
+            }
+        }
+
+        if(newPageNumber === "up"){
+            if((pageNum + 1) <= maxPageNum) {
+                pageNum++;
+                this.setState({
+                    page:pageNum
+                })
+            }
+        }
+        if(typeof newPageNumber === "number"){
+            this.setState({
+                page:newPageNumber
+            })
+        }
+    }
+
     render() {
         return (
             <div className="repoList">
-                <PaginationElement numberOfPages={this.state.numberOfPages} page={this.state.page} />
+                <PaginationElement numberOfPages={this.state.numberOfPages} page={this.state.page} changePage={this.changePage} />
+                    {this.props.loading === true && <div><h1>LOAAAAAADDING</h1></div>}
                     {this.props.repos.length === 0 && <BlankList />}
                     {this.props.repos && this.props.repos.map((repo, key)=>{
                         return (
                             <Repo repo={repo} key={key}/>
                         )
                     })}
-                    <PaginationElement numberOfPages={this.state.numberOfPages} page={this.state.page} />
+                    <PaginationElement numberOfPages={this.state.numberOfPages} page={this.state.page} changePage={this.changePage} />
             </div>
         )
     }
