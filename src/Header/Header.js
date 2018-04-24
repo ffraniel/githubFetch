3f5b362ec9e731daeb5fdcea47b80f3297d482a8
ffraniel <francis.whitehead@googlemail.com>
2018-04-24 13:14:00 +0100

    added link back to home to header logo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './Header.css';
 import Search from '../Search/Search';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     render(){
         return(
         <header className="header">
-          <img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="github logo" />
+          <Link to={{pathname:`/`}}>
+            <img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="github logo" />
+          </Link>
           <Search handleChange={this.props.handleChange} handleSubmit={this.props.handleSubmit} />
         </header>
         )
